395bd62d82f0e1c8e0a42cbcbf7fa52aaaa5f6bf
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-24 17:19:41 -0700

    Set up GroupController and its CRUD functions

--- AddressBook/AddressBook/Controllers/ModelControllers/GroupController.swift
+++ AddressBook/AddressBook/Controllers/ModelControllers/GroupController.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+class GroupController {
+    
+    // MARK: - Properties
+    static var sharedInstance = GroupController()
+    var groups: [Group] = []
+    
+    // MARK: - Initializers
+    init() {
+        loadContactsFromDisk()
+    }
+    
+    // MARK: - CRUD FUNCS
+    
+    func createGroup(name: String = "Untitled Group", people:[Person] = []) {
+        let group = Group(name: name, people: people)
+        groups.append(group)
+        saveContactsToDisk()
+    }
+    
+    func updateGroup(group: Group, name: String, people: [Person]) {
+        group.name = name
+        group.people = people
+        saveContactsToDisk()
+    }
+    
+    func deleteGroup(group: Group) {
+        guard let index = groups.firstIndex(of: group) else { return }
+        groups.remove(at: index)
+        saveContactsToDisk()
+    }
+    
+    func saveContactsToDisk() {
+        //Get address to save to
+        guard let url = fileURL else { return }
+        do {
+        //Convert/Encode
+            let data = try JSONEncoder().encode(groups)
+        //Save the data to the address
+            try data.write(to: url)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func loadContactsFromDisk() {
+        //Get the address your data is saved at
+        guard let url = fileURL else { return }
+        do {
+        //Load the data from the address
+        let data = try Data(contentsOf: url)
+        //decode that data into our swift model object
+            let groups = try JSONDecoder().decode([Group].self, from: data)
+            self.groups = groups
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    // MARK: - Computed Property
+    private var fileURL: URL? {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let url = documentsDirectory.appendingPathComponent("addressbook.json")
+        return url
+    }
+    
+} //End of class
+
+
+
+
+
+
--- AddressBook/AddressBook/Controllers/ModelControllers/PersonController.swift
+++ AddressBook/AddressBook/Controllers/ModelControllers/PersonController.swift
@@ -14,16 +14,22 @@ class PersonController {
     static func createPerson(name: String = "New Contact", address: String = "", group: Group) {
         let person = Person(name: name, address: address)
         group.people.append(person)
+        
+        //TODO: Save to persistent
     }
     
     static func deletePerson(person: Person, group: Group) {
         guard let index = group.people.firstIndex(of: person) else { return }
         group.people.remove(at: index)
+        
+        //TODO: Save to persistent
     }
     
     static func updatePerson(person: Person, name: String, address: String) {
         person.address = address
         person.name = name
+        
+        //TODO: Save to persistent
     }
     
 }
