959a1aeee5c06a58fbc4aa3fd5a2e4ba7421f262
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-25 17:15:27 -0700

    integrated functionaility for the save details button

--- AddressBook/AddressBook/Controllers/ViewControllers/PersonDetailViewController.swift
+++ AddressBook/AddressBook/Controllers/ViewControllers/PersonDetailViewController.swift
@@ -8,24 +8,49 @@
 import UIKit
 
 class PersonDetailViewController: UIViewController {
-
+    
     // MARK: -Properties
     var person: Person?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
- 
+    // MARK: -IBOutlets
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    // MARK: - LifeCycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
     }
-    */
-
+    func updateViews() {
+        guard let person = person else { return }
+        nameTextField.text = person.name
+        addressTextField.text = person.address
+    }
+    // MARK: -IBActions
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        guard let name = nameTextField.text,
+              let address = addressTextField.text else { return }
+        if let person = person {
+            PersonController.updatePerson(person: person, name: name, address: address)
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
 }
